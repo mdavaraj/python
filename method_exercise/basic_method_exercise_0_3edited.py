47cb9b3e2351d2f3f8169c0c0020217278962eb3
Mark Davaraj <mdavaraj@gmail.com>
2023-05-13 14:46:30 +0800

    updates

---
+++ method_exercise/basic_method_exercise_0_3edited.py
@@ -0,0 +1,62 @@
+# Objective  
+# Ability to understand function 
+# Ability to improve previous exercise
+
+# What to do
+# ==========
+# Follow TODO below
+
+
+# TODO refer to the https://github.com/pcyuen98/python-intro/blob/main/src/exercise/core/input/exercise_basic_input_1.py
+# 1. Complete the static function getTotal Price 
+# 2. Create another py file as input file and this file function 
+
+def getTotalPrice(x):
+# Complete your code below
+
+    if (x == "1"):
+        price = 0 + 10
+        return price;
+
+    elif (x == "2"):
+        price = 0 + 2
+        return price;
+    elif (x == "3"):
+        price = 0 + 1
+        return price;
+    else:
+        print("Invalid choice selected!")
+        price = 0 + int(x)
+        return price;
+
+
+food_1 = "Mcdonald"
+food_price_1 = "RM 10"
+food_2 = "Nasi Lemak"
+food_price_2 = "RM 2"
+food_3 = "Mee Kosong"
+food_price_3 = "RM 1"
+
+
+print ("Food 1. Mcdonald RM 10")
+print ("Food 2. Nasi Lemak RM 2")
+print ("Food 3. Mee Kosong RM 1")
+
+x = input("Enter your food choice here: Enter 1 2 or 3 ")
+getTotalPrice(x)
+
+print ('Your selected food is ' + x + ' and the total price is RM' , getTotalPrice(x))
+
+# if (x == "1"):
+# 	print ('Your selected food is ' + food_1 + ' and the total price is ' + food_price_1)
+#
+# elif (x == "2"):
+# 	print ('Your selected food is ' + food_2 + ' and the total price is ' + food_price_2)
+# elif (x == "3"):
+# 	print ('Your selected food is ' + food_3 + ' and the total price is ' + food_price_3)
+# else:
+# 	print("Invalid choice selected!")
+
+# modify your previous exercise by adding a function below
+    
+# print ('Your selected food is ' + x + ' and the total price is RM' , getTotalPrice(x)  
